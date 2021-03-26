425d96c3cc2b1cbd83c6809188b25f90014abc16
01pooja10 <>
2021-03-26 19:18:54 +0530

    Updated audio file
    Updated audio file

--- easydocs/ocr&audio/audiobook.py
+++ easydocs/ocr&audio/audiobook.py
@@ -4,35 +4,28 @@ import json
 import os
 import pyttsx3
 import PyPDF2
+import streamlit as st
 
-print('Welcome to the 3rd section of the app: PDF to audiobook converter')
-print('Upload any pdf file:')
-print('ProTip: To convert an image to a text file, use the OCR functionality provided by EasyDocs :)')
+st.title('Hello there!')
+st.subheader('A simple text-to-speech converter.')
+#pdf='Hey there pooja. how are you?'
+inp=st.text_area('Enter text: ')
+inp1=st.radio('Do you want to save the output file?',('Yes','No'))
+if inp1=='Yes':
+    inp2=st.radio('Do you want it read slowly?',('Yes','No'))
+    if inp2=='Yes':
+        result=gTTS(text=inp,lang='en',slow=True)
+    elif inp2=='No':
+        result=gTTS(text=inp,lang='en',slow=False)
 
-#Have to add functionality to accept either pdf or txt file
-#path=r'C:/Users/Pooja/Documents/ocr_google/stuff/oops_pdfe.pdf'
-def convert():
-    with open(path,'rb') as fla:
-        pdf=PyPDF2.PdfFileReader(fla)
+    result.save('your_audiobook.mp3')
 
-    inp=input('Do you want the document to be read in a slower pace? Y/N')
-    if inp=='Y':
-        result=gTTS(text=pdf,lang='en',slow=True)
-    elif inp=='N':
-        result=gTTS(text=pdf,lang='en',slow=False)
-    else:
-        print('Enter only Y/N for yes or no.')
-
-    inp1=input('Do you want to save the output file? Y/N')
-    if inp1=='Y' or inp1=='y':
-        result.save('your_audiobook.mp3')
-
-    elif inp1=='N' or inp1=='n':
-        page=pdf.getPage(1)
-        content=pdf.extractText(page)
-        speaker=pyttsx3.init()
-        speaker.say(content)
-        speaker.runAndWait()
-
-    else:
-        print('Invalid entry :(')
+elif inp1=='No':
+    #numpgs=pdf.numPages
+    #print(numpgs)
+    #for i in range(numpgs):
+        #page=pdf.getPage(i)
+        #content += page.extractText()
+    speaker=pyttsx3.init()
+    speaker.say(inp)
+    speaker.runAndWait()
