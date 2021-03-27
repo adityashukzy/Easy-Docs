edcc5486df962d8845e6a08a37b047522ba43d50
01pooja10 <>
2021-03-27 14:42:25 +0530

    Updated response.py

--- easydocs/ezpz/response.py
+++ easydocs/ezpz/response.py
@@ -19,7 +19,7 @@ def input_bag(sen, words):
 	bag = [0] * len(words)
 	wrds = nltk.word_tokenize(sen)
 
-	wrds = [lemm.lemmatize(w.lower()) for w in words]
+	wrds = [lemm.lemmatize(w.lower()) for w in wrds]
 
 	for s in wrds:
 		for i, j in enumerate(wrds):
