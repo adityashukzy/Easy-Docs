b3829147b8ffc9f118aa2356759569c8dabcaa7e
Aditya Shukla <adityashukzy@gmail.com>
2021-03-27 14:06:16 +0530

    Minor

--- easydocs/ocr/ocr.py
+++ easydocs/ocr/ocr.py
@@ -4,7 +4,7 @@ import easyocr
 def transcribe_image(image):
 	image = Image.open(image)
 	reader = easyocr.Reader(['en'])
-	result = reader.readtext(image, detail=0)
+	result = reader.readtext(r'dependencies/lstm_cell.jpg', detail=0)
 
 	return "\n".join(result)
 
