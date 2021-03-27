7523b64f0c755d83695f22460bec7cf53fe7fa80
Aditya Shukla <adityashukzy@gmail.com>
2021-03-27 22:54:07 +0530

    Minor fix

--- st_app.py
+++ st_app.py
@@ -66,10 +66,10 @@ def main():
 		st.image(img_path,width = 500)
 		st.subheader("EasyDocs is a one-stop solution combining all your most needed tools as a student. We understand the value and preciousness of time and that is why we have aimed to make EasyDocs as no-nonsense as possible!")
 
-		st.write("-- Summarize webpages and long text documents.")
-		st.write("-- Scan images for text and have them transcribed. This is called Optical Character Recognition or OCR!")
-		st.write("-- Play around with and explore the extremely convenient method of learning: *audiobooks*!")
-		st.write("If you're just looking around, maybe visit the EzPz our chatbot and learn more about EasyDocs!")
+		st.write("👉 Summarize webpages and long text documents and breeze through the essentials!")
+		st.write("👉 Scan images for text and have them transcribed. This is called Optical Character Recognition!")
+		st.write("👉 Play around with and explore the extremely convenient method of learning: *audiobooks*!")
+		st.write("👉 If you're just looking around, maybe visit EzPz our chatbot and learn about EasyDocs!")
 
 	elif option == 'Summarize URL':
 		## URL summarization
